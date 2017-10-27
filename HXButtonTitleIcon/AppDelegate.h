16163bc6f378f05144835d782af1696cdd8c06e9
mars <627556038@qq.com>
2017-10-27 08:43:12 +0800

    Initial Commit

---
+++ HXButtonTitleIcon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXButtonTitleIcon
+//
+//  Created by 郭海祥 on 2017/10/27.
+//  Copyright © 2017年 ghx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXButtonTitleIcon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXButtonTitleIcon
+//
+//  Created by 郭海祥 on 2017/10/27.
+//  Copyright © 2017年 ghx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
